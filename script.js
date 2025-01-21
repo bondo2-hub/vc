2a817c9316e252d972477287a1fc44a64c5ad06c
bondo2 <ahmedthek3@gmail.com>
2025-01-21 15:07:16 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+    const body = document.getElementById("body");
+
+    hamburgerMenu.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (dropdownMenu.style.display === "flex") {
+            dropdownMenu.style.display = "none";
+            body.style.overflow = "";
+        } else {
+            dropdownMenu.style.display = "flex";
+            body.style.overflow = "hidden";
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const boxes = document.querySelector('.boxes');
+    const leftBtn = document.querySelector('.left-btn');
+    const rightBtn = document.querySelector('.right-btn');
+    let boxWidth = window.innerWidth;
+
+    leftBtn.addEventListener('click', function() {
+        boxes.scrollLeft -= 335;
+    });
+
+    rightBtn.addEventListener('click', function() {
+        boxes.scrollLeft += 335;
+    });
+
+    window.addEventListener('resize', function() {
+        boxWidth = window.innerWidth;
+    });
+});
+
+
+
